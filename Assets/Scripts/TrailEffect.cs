67877640f4b10c737e8bf378c74b088b4ea223c7
UnityLV <104660372+UnityLV@users.noreply.github.com>
2023-08-28 21:57:18 +0300

    Add Trail
    All particle system as trail

--- Assets/Scripts/TrailEffect.cs
+++ Assets/Scripts/TrailEffect.cs
@@ -1,18 +1,21 @@
 ﻿using UnityEngine;
 
-public class TrailEffect : MonoBehaviour {
-    public TrailRenderer trailRenderer;
+public class TrailEffect : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem _particleSystem;
+    [SerializeField] private Rigidbody2D _rigidbody2D;
+    [SerializeField] private float _scaler;
+    private ParticleSystem.VelocityOverLifetimeModule _velocityModule;
 
-    void Start() {
-        trailRenderer = GetComponent<TrailRenderer>();
+    private void Start()
+    {
+        _velocityModule = _particleSystem.velocityOverLifetime;
     }
 
-    void Update() {
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            trailRenderer.emitting = true;
-        }
-        if (Input.GetKeyUp(KeyCode.Space)) {
-            trailRenderer.emitting = false;
-        }
+    private void Update()
+    {
+        float verticalVelocity = _rigidbody2D.velocity.y;
+
+        _velocityModule.yMultiplier = verticalVelocity * _scaler;
     }
 }
