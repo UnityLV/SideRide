c822b8227b16cbcac4cf80c592b3cf0d0de6b4fa
UnityLV <104660372+UnityLV@users.noreply.github.com>
2023-08-28 21:26:29 +0300

    Import
    TMP

--- Assets/Scripts/GameStarter.cs
+++ Assets/Scripts/GameStarter.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.Events;
 
 
 public class GameStarter : MonoBehaviour
 {
+    [SerializeField] private UnityEvent _onGameStarted;
     public bool IsGameStarted { get; private set; }
     public void StartGame()
     {
         IsGameStarted = true;
         gameObject.SetActive(true);
+        _onGameStarted?.Invoke();
         Debug.Log("Game started");
     }
 }
