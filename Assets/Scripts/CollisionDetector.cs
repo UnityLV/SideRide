33d434ce1eaab97cf0a19696573bfd4dc53e512e
UnityLV <104660372+UnityLV@users.noreply.github.com>
2023-08-29 08:25:04 +0300

    Add
    End Game Detector

---
+++ Assets/Scripts/CollisionDetector.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class CollisionDetector : MonoBehaviour
+{
+    [SerializeField] private UnityEvent _onCollided;
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        _onCollided?.Invoke();
+    }
+
+}
--- Assets/Scripts/SmoothFollow.cs
+++ Assets/Scripts/SmoothFollow.cs
@@ -1,20 +1,20 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class SmoothFollow : MonoBehaviour
 {
-    [SerializeField] private Transform _target; // Второй объект, за которым нужно двигаться
-    [SerializeField] private float _smoothSpeed = 5f; // Скорость плавного движения
+    [SerializeField] private Transform _target;
+    [SerializeField] private float _smoothSpeed = 5f; 
 
     private void Update()
     {
-        // Проверяем, что есть цель для следования
         if (_target != null)
         {
-            // Вычисляем новую позицию текущего объекта, плавно двигая его к цели
             Vector3 newPosition = Vector3.Lerp(transform.position, _target.position, _smoothSpeed * Time.deltaTime);
 
-            // Применяем новую позицию
             transform.position = newPosition;
         }
     }
+    
+
 }
